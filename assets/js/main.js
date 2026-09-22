bfc0bc638beca78d6aa95bc9d8f7a3dff31b362e
Scarlett Alonso <dal130921@utdallas.edu>
2026-09-22 13:56:47 +0000

    fixed "test" to text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 // main.js
 
-console.log("Hello, World!");
+let targetElement = document.querySelector("h2");
+
+targetElement.addEventListener("mouseover", function(){
+    targetElement.innerText= "TIME";
+    targetElement.style.cursor = "wait";
+});
+
+targetElement.addEventListener("mouseout", function(){
+    targetElement.innerText= "IKEA";
+    targetElement.style.cursor = "pointer";
+});
